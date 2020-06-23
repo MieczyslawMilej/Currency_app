70362d1e580e19cdae16b1bb0f50e160ecc13309
MieczyslawMilej <mieczyslaw.milej@gmail.com>
2020-06-23 11:26:03 +0200

    Updates

---
+++ precache-manifest.f311a79610c9c4400f0ca655676d21f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a1190b99da119cd6282ac3978806e2",
+    "url": "/Currency_app/index.html"
+  },
+  {
+    "revision": "f12f5e9b006bf89f5939",
+    "url": "/Currency_app/static/css/main.60f42147.chunk.css"
+  },
+  {
+    "revision": "63769a3196f087a60912",
+    "url": "/Currency_app/static/js/2.0b31d353.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Currency_app/static/js/2.0b31d353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f12f5e9b006bf89f5939",
+    "url": "/Currency_app/static/js/main.fae0f62c.chunk.js"
+  },
+  {
+    "revision": "8ca97cee79d29bb8d5e2",
+    "url": "/Currency_app/static/js/runtime-main.01bf1ca4.js"
+  }
+]);
